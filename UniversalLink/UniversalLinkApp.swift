26e9dc3103d6ffd4ac3708ab039035a8d7d9866f
Jeff <>
2022-07-18 17:30:57 +0800

    Added project

---
+++ UniversalLink/UniversalLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalLinkApp.swift
+//  UniversalLink
+//
+//  Created by Jeffrey Tabios on 7/15/22.
+//
+
+import SwiftUI
+
+@main
+struct UniversalLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
