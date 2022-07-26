83c39131f97e8101f4c307bb19978e8d989b0651
Jeff <>
2022-07-26 18:01:58 +0800

    added configs

--- UniversalLink/ContentView.swift
+++ UniversalLink/ContentView.swift
@@ -6,11 +6,60 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
+    @State var phone = "+19482840294"
+    @State var success = ""
+    @State var error = ""
     var body: some View {
-        Text("Welcome")
-            .padding()
+        VStack {
+            Spacer()
+            Text("Send SMS to login:")
+            TextField("+639175361205", text: $phone)
+                .padding(5)
+                .frame(width: 200)
+                .border(.blue, width: 2)
+            HStack {
+                Button(action: {
+                    PhoneAuthProvider.provider()
+                      .verifyPhoneNumber(phone, uiDelegate: nil) { verificationID, error in
+                          if let error = error {
+                              self.error = error.localizedDescription
+                            return
+                          }
+                          self.success = "SUCCESS"
+                      }
+                }) {
+                    Text("Send SMS")
+                        .padding(8)
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .cornerRadius(14)
+                        
+                }
+                
+                Button(action: {
+                    self.success = ""
+                    self.error = ""
+                }) {
+                    Text("Reset")
+                        .padding(8)
+                        .foregroundColor(.white)
+                        .background(.red)
+                        .cornerRadius(14)
+                        
+                }
+            }
+            Text(error)
+                .foregroundColor(.red)
+            Text(success)
+                .foregroundColor(.blue)
+            Spacer()
+            
+            Spacer()
+        }
+        
     }
 }
 
--- UniversalLink/UniversalLinkApp.swift
+++ UniversalLink/UniversalLinkApp.swift
@@ -6,12 +6,32 @@
 //
 
 import SwiftUI
-
+import Firebase
 @main
 struct UniversalLinkApp: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+//        Auth.auth().setAPNSToken(deviceToken, type: AuthAPNSTokenType.sandbox)
+    }
+
+    func application(_ application: UIApplication , didReceiveRemoteNotification notification: [AnyHashable : Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void)
+    {
+        if Auth.auth().canHandleNotification(notification)
+        {
+            completionHandler(UIBackgroundFetchResult.noData);
+            return
         }
     }
 }
